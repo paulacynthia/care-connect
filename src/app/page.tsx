1bc15e0550b9a7b717c60c3d7ffff1cadc98e925
paulacynthia <pcynthia013@gmail.com>
2024-07-19 12:17:17 -0300

    :sparkles: feat: page: home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,32 @@
+import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
 import { HealthUnit } from "@/components/HealthUnit";
-import { Flex } from "@chakra-ui/react";
+import { SearchForm } from "@/components/SearchForm";
+import { Box, Flex, VStack } from "@chakra-ui/react";
 
 export default function Home() {
   return (
-    <>
+    <Flex flexDirection={"column"} height={"100vh"}>
       <Header />
-      <Flex width="full" justifyContent={"center"} flexDirection={"column"}>
-        <HealthUnit />
-        <HealthUnit />
-        <HealthUnit />
-        <HealthUnit />
-      </Flex>
-    </>
+      <VStack
+        flex={1}
+        p={["0.5rem", "1rem"]}
+        width={"full"}
+        maxWidth={"150rem"}
+        overflow={["none", "auto"]}
+        spacing={"1.5rem"}
+      >
+        <SearchForm />
+        <Flex gap="1rem" flexWrap={"wrap"} justifyContent={"center"}>
+          <HealthUnit />
+          <HealthUnit />
+          <HealthUnit />
+          <HealthUnit />
+          <HealthUnit />
+          <HealthUnit />
+        </Flex>
+      </VStack>
+      <Footer />
+    </Flex>
   );
 }
