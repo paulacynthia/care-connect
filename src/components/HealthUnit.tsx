6e48d8433b109a7412e6e853eaed8b31758fd806
paulacynthia <pcynthia013@gmail.com>
2024-07-19 12:21:26 -0300

    :sparkles: feat: health unit

--- src/components/HealthUnit.tsx
+++ src/components/HealthUnit.tsx
@@ -2,7 +2,7 @@
 
 import {
   Avatar,
-  Box,
+  Button,
   Card,
   CardBody,
   CardFooter,
@@ -14,97 +14,84 @@ import {
   Text,
   VStack,
 } from "@chakra-ui/react";
-import { Star } from "lucide-react";
+import { ArrowRight, Star } from "lucide-react";
 
 export function HealthUnit() {
   return (
-    <Box p="8px" minWidth="100%">
-      <Card background={"white"} p="0.4rem">
-        <CardHeader p="0.4rem">
-          <Flex
-            width="full"
-            alignItems={"center"}
-            justifyContent={"space-between"}
-            gap="4px"
-          >
-            <Avatar
-              borderRadius={"md"}
-              name="Registered Nurse"
-              size={"md"}
-              bgColor="blueX.500"
-              color="white"
-            />
+    <Card p="8px" minWidth={["full", "25rem"]} background={"white"}>
+      <CardHeader p="0.4rem">
+        <Flex
+          width="full"
+          alignItems={"center"}
+          justifyContent={"space-between"}
+          gap="4px"
+        >
+          <Avatar
+            borderRadius={"md"}
+            name="Registered Nurse"
+            size={"md"}
+            bgColor="blueX.500"
+            color="white"
+          />
 
-            {/* <Image
-              src={"/assets/test.jpeg"}
-              alt=""
-              width={80}
-              height={80}
-              quality={100}
-              
-            /> */}
-            <Text fontSize={"12px"} color={"grayX.500"}>
-              4 dias atrás
+          <Text fontSize={"12px"} color={"grayX.500"}>
+            4 dias atrás
+          </Text>
+        </Flex>
+      </CardHeader>
+      <CardBody p="0.4rem">
+        <VStack alignItems={"flex-start"} gap="5px">
+          <Text fontWeight={"semibold"}>Registered Nurse - CVICU</Text>
+          <Text fontSize={"14px"}>
+            Children&lsquo;s Hospital Of Los Angeles
+          </Text>
+          <Text fontSize={"14px"}>Los Angeles, CA</Text>
+        </VStack>
+      </CardBody>
+      <CardFooter width="full" p="0.5rem" justifyContent={"flex-start"}>
+        <VStack
+          width="full"
+          alignItems={"flex-start"}
+          justifyContent={"flex-end"}
+        >
+          <Flex alignItems={"center"} gap="0.5rem">
+            <Text fontSize={"14px"} fontWeight={"semibold"} color={"blueX.900"}>
+              R$ 50/hora
             </Text>
+            4.5
+            <Icon
+              color={"yellow.400"}
+              w={4}
+              h={4}
+              as={Star}
+              fill={"yellow.400"}
+            />
           </Flex>
-        </CardHeader>
-        <CardBody p="0.4rem">
-          <VStack alignItems={"flex-start"} gap="5px">
-            <Text fontWeight={"semibold"}>Registered Nurse - CVICU</Text>
-            <Text fontSize={"14px"}>
-              Children&lsquo;s Hospital Of Los Angeles
-            </Text>
-            <Text fontSize={"14px"}>Los Angeles, CA</Text>
-          </VStack>
-        </CardBody>
-        <CardFooter p="0.5rem" justifyContent={"flex-end"}>
-          <VStack alignItems={"flex-end"} justifyContent={"flex-end"}>
-            <Flex alignItems={"center"}  gap="0.5rem">
-              <Text
-                fontSize={"14px"}
-                fontWeight={"semibold"}
-                color={"blueX.900"}
-              >
-                $ 48 / hour
-              </Text>
-              <Icon
-                color={"yellow.400"}
-                w={4}
-                h={4}
-                as={Star}
-                fill={"yellow.400"}
-              />{" "}
-              4.5
-            </Flex>
-            <HStack>
-              <Tag
-                size="sm"
-                borderRadius={"sm"}
-                variant="subtle"
-                colorScheme="blue"
-              >
-                STAFF
-              </Tag>
-              <Tag
-                size="sm"
-                borderRadius={"sm"}
-                variant="subtle"
-                colorScheme="red"
-              >
-                DAYS
-              </Tag>
-              <Tag
-                size="sm"
-                borderRadius={"sm"}
-                variant="subtle"
-                colorScheme="green"
-              >
-                FLEXIBLE START
-              </Tag>
-            </HStack>
-          </VStack>
-        </CardFooter>
-      </Card>
-    </Box>
+
+          <HStack width="full" justifyContent={"space-between"}>
+            <Tag
+              size="sm"
+              padding={"0.2rem"}
+              borderRadius={"sm"}
+              variant="subtle"
+              colorScheme="green"
+            >
+              3 TURNOS DISPONÍVEIS
+            </Tag>
+
+            <Button
+              variant={"outline"}
+              size="sm"
+              borderRadius={"md"}
+              borderColor="blueX.500"
+              color="blueX.500"
+              rightIcon={<ArrowRight size="20px" />}
+            >
+              Ver mais
+            </Button>
+          </HStack>
+        </VStack>
+      </CardFooter>
+    </Card>
   );
 }
