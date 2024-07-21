59822fa6d4d9bebcccdaee4cf483824f0b9554b3
paulacynthia <pcynthia013@gmail.com>
2024-07-21 18:22:03 -0300

    :ok_hand: style: double quotes

--- src/data/api.ts
+++ src/data/api.ts
@@ -1,22 +1,25 @@
-export function api(path: string, init?: RequestInit) {
-  const baseUrl = process.env.NEXT_PUBLIC_API_URL
-  const apiPrefix = '/api'
+import { ReadonlyHeaders } from "next/dist/server/web/spec-extension/adapters/headers";
 
-  const url = new URL(apiPrefix.concat(path), baseUrl)
-  return fetch(url, init)
+export function api(path: string, headers?: ReadonlyHeaders, init?: RequestInit) {
+  const baseUrl = process.env.NEXT_PUBLIC_API_URL;
+  const apiPrefix = "/api";
+
+  const url = new URL(apiPrefix.concat(path), baseUrl);
+  return fetch(url, {
+    headers
+  });
 }
 
 export function post(path: string, jsonBody: object) {
-  const baseUrl = process.env.NEXT_PUBLIC_API_URL
-  const apiPrefix = '/api'
+  const baseUrl = process.env.NEXT_PUBLIC_API_URL;
+  const apiPrefix = "/api";
 
-  const url = new URL(apiPrefix.concat(path), baseUrl)
+  const url = new URL(apiPrefix.concat(path), baseUrl);
   return fetch(url, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify(jsonBody)
-  })
+    body: JSON.stringify(jsonBody),
+  });
 }
-
