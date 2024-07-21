aab520e45ce9d5350dd4d12fdc5182ed53c0dc47
paulacynthia <pcynthia013@gmail.com>
2024-07-21 18:22:43 -0300

    :sparkles: feat: drawer filter

---
+++ src/components/DrawerFilter.tsx
@@ -0,0 +1,152 @@
+"use client";
+
+import {
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  Flex,
+  HStack,
+  NumberDecrementStepper,
+  NumberIncrementStepper,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  Select,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import { useRouter } from "next/navigation";
+import { FormEvent } from "react";
+
+interface DrawerFilterProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+export function DrawerFilter({ isOpen, onClose }: DrawerFilterProps) {
+  const router = useRouter();
+
+  function handleFilter(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    const data = Object.fromEntries(formData);
+
+    const query = Object.entries(data)
+      .filter(([_, value]) => value)
+      .map(([key, value]) => `${key}=${value}`)
+      .join("&");
+
+    router.replace(`/search?${query}`);
+    onClose();
+  }
+
+  return (
+    <Drawer isOpen={isOpen} placement="right" onClose={onClose} size="sm">
+      <DrawerOverlay />
+      <DrawerContent>
+        <form onSubmit={handleFilter}>
+          <DrawerCloseButton />
+          <DrawerHeader>Filtros:</DrawerHeader>
+
+          <DrawerBody>
+            <Flex width="full" gap="1rem" flexDirection={"column"}>
+              <Flex width="full" gap="0.5rem" flexDirection={"row"}>
+                <VStack width="full" alignItems={"flex-start"}>
+                  <Text>Hora inicial:</Text>
+                  <NumberInput width="full" step={1} min={0} max={24}>
+                    <NumberInputField name="initialHour" />
+                    <NumberInputStepper>
+                      <NumberIncrementStepper />
+                      <NumberDecrementStepper />
+                    </NumberInputStepper>
+                  </NumberInput>
+                </VStack>
+                <VStack width="full" alignItems={"flex-start"}>
+                  <Text>Hora final:</Text>
+                  <NumberInput width="full" step={1} min={0} max={24}>
+                    <NumberInputField name="finishHour" />
+                    <NumberInputStepper>
+                      <NumberIncrementStepper />
+                      <NumberDecrementStepper />
+                    </NumberInputStepper>
+                  </NumberInput>
+                </VStack>
+              </Flex>
+
+              <VStack width="full" alignItems={"flex-start"}>
+                <Text>Avaliação mínima:</Text>
+                <NumberInput width="full" step={0.25} min={0} max={5}>
+                  <NumberInputField name="minimumScore" />
+                  <NumberInputStepper>
+                    <NumberIncrementStepper />
+                    <NumberDecrementStepper />
+                  </NumberInputStepper>
+                </NumberInput>
+              </VStack>
+
+              <VStack width="full" alignItems={"flex-start"}>
+                <Text>Remuneração mínima:</Text>
+                <NumberInput width="full" step={15} min={10}>
+                  <NumberInputField name="minimumPayment" />
+                  <NumberInputStepper>
+                    <NumberIncrementStepper />
+                    <NumberDecrementStepper />
+                  </NumberInputStepper>
+                </NumberInput>
+              </VStack>
+
+              <Flex gap="0.5rem" flexDirection={"column"}>
+                <Text>Ordenar por:</Text>
+                <HStack
+                  gap={["1rem", "0.5rem"]}
+                  flexDirection={["column", "row"]}
+                  width="full"
+                >
+                  <Select
+                    name="orderBy"
+                    borderColor="gray.200"
+                    background={"white"}
+                    minWidth="70%"
+                  >
+                    <option selected hidden disabled value="">
+                      Campo
+                    </option>
+                    <option value="payment">Remuneração</option>
+                    <option value="grade">Nota</option>
+                    <option value="date">Data de postagem</option>
+                  </Select>
+                  <Select
+                    name="orderType"
+                    borderColor="gray.200"
+                    background={"white"}
+                  >
+                    <option selected hidden disabled value="">
+                      Ordem
+                    </option>
+                    <option value="desc">Desc &#8595;</option>
+                    <option value="asc">Asc &#8593;</option>
+                  </Select>
+                </HStack>
+              </Flex>
+            </Flex>
+          </DrawerBody>
+
+          <DrawerFooter>
+            <Button variant="outline" mr={3} onClick={onClose}>
+              Cancelar
+            </Button>
+            <Button type="submit" variant={"custom"} color="white">
+              Filtrar
+            </Button>
+          </DrawerFooter>
+        </form>
+      </DrawerContent>
+    </Drawer>
+  );
+}
--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -7,12 +7,16 @@ import {
   Input,
   InputGroup,
   InputRightElement,
-  useDisclosure
+  useDisclosure,
 } from "@chakra-ui/react";
-import { Search } from "lucide-react";
+import { Filter, Search } from "lucide-react";
+import { useRouter } from "next/navigation";
 import { FormEvent } from "react";
+import { DrawerFilter } from "./DrawerFilter";
 
 export function SearchForm() {
+  const router = useRouter();
+
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   function handleSearch(event: FormEvent<HTMLFormElement>) {
@@ -22,130 +26,76 @@ export function SearchForm() {
     const data = Object.fromEntries(formData);
 
     const query = data.q;
-    console.log("query", query);
+    router.push(`/search?q=${query}`);
   }
 
   return (
-    <Box as="form" onSubmit={handleSearch} display={"flex"} justifyContent={"center"}>
-      <Flex
-        flexDirection={["column", "row"]}
-        width="full"
-        maxWidth="45rem"
-        alignItems={"center"}
-        gap="0.5rem"
+    <Flex
+      width="full"
+      gap="0.5rem"
+      justifyContent="center"
+      alignItems="center"
+      px="0.5rem"
+    >
+      <Box
+        as="form"
+        onSubmit={handleSearch}
+        display={"flex"}
+        justifyContent={"center"}
       >
-        <InputGroup size="md" width="full" gap="0.5rem">
-          <Input
-            type="text"
-            name="q"
-            borderColor={"gray.200"}
-            background={"white"}
-            placeholder="Pesquisar"
-            _placeholder={{ opacity: 1, color: "gray.500" }}
-            required
-            maxWidth={"full"}
-          />
-          <InputRightElement position={"relative"}>
-            <IconButton
-              type="submit"
-              aria-label="Filter"
-              h="full"
-              w="full"
-              size="sm"
-              icon={<Search color="#184796" size={"1rem"} />}
-              color={"gray.200"}
+        <Flex
+          flexDirection={["column", "row"]}
+          width="full"
+          maxWidth="45rem"
+          alignItems={"center"}
+          gap="0.5rem"
+        >
+          <InputGroup size="md" width="full" gap="0.5rem">
+            <Input
+              type="text"
+              name="q"
+              borderColor={"gray.200"}
               background={"white"}
-              border={"1px solid"}
-              position={"relative"}
-              _hover={{}}
+              placeholder="Pesquisar"
+              _placeholder={{ opacity: 1, color: "gray.500" }}
+              maxWidth={"full"}
             />
-          </InputRightElement>
-        </InputGroup>
+            <InputRightElement position={"relative"}>
+              <IconButton
+                type="submit"
+                aria-label="Filter"
+                h="full"
+                w="full"
+                size="sm"
+                icon={<Search color="#184796" size={"1rem"} />}
+                color={"gray.200"}
+                background={"white"}
+                border={"1px solid"}
+                position={"relative"}
+                _hover={{}}
+              />
+            </InputRightElement>
+          </InputGroup>
+        </Flex>
+      </Box>
 
-        {/* <IconButton
-          aria-label="Filter"
-          background={"white"}
-          icon={<Filter color="#184796" size={"1rem"} />}
-          onClick={onOpen}
-          variant={"outline"}
-        />
+      <IconButton
+        aria-label="Filter"
+        background={"white"}
+        icon={<Filter color="#184796" size={"1rem"} />}
+        h="full"
+        w="auto"
+        variant={"outline"}
+        onClick={onOpen}
+        type="submit"
+        size="sm"
+        color={"gray.200"}
+        border={"1px solid"}
+        position={"relative"}
+        _hover={{}}
+      />
 
-        <Drawer isOpen={isOpen} placement="right" onClose={onClose} size="sm">
-          <DrawerOverlay />
-          <DrawerContent>
-            <DrawerCloseButton />
-            <DrawerHeader>Filtros:</DrawerHeader>
-
-            <DrawerBody>
-              <Flex width="full" gap="1rem" flexDirection={"column"}>
-                <Flex gap="0.5rem" flexDirection={"column"}>
-                  <Text>Geral:</Text>
-                  <Select borderColor="gray.200" background={"white"}>
-                    <option selected hidden disabled value="">
-                      Selecione uma ordenação
-                    </option>
-                    <option value="payment-ASC">Maior remuneração</option>
-                    <option value="grade-ASC">Maior nota</option>
-                    <option value="date-ASC">Mais recente</option>
-                    <option value="date-DESC">Mais antigo</option>
-                  </Select>
-                </Flex>
-
-                <Flex width="full" gap="0.5rem" flexDirection={"row"}>
-                  <VStack width="full" alignItems={"flex-start"}>
-                    <Text>Data inicial:</Text>
-                    <Input
-                      placeholder="Select Date and Time"
-                      size="md"
-                      type="date"
-                    />
-                  </VStack>
-
-                  <VStack width="full" alignItems={"flex-start"}>
-                    <Text>Data final:</Text>
-                    <Input
-                      placeholder="Select Date and Time"
-                      size="md"
-                      type="date"
-                    />
-                  </VStack>
-                </Flex>
-
-                <VStack width="full" alignItems={"flex-start"}>
-                  <Text>Avaliação mínima:</Text>
-                  <NumberInput width="full" step={0.25} min={0} max={5}>
-                    <NumberInputField />
-                    <NumberInputStepper>
-                      <NumberIncrementStepper />
-                      <NumberDecrementStepper />
-                    </NumberInputStepper>
-                  </NumberInput>
-                </VStack>
-
-                <VStack width="full" alignItems={"flex-start"}>
-                  <Text>Remuneração mínima:</Text>
-                  <NumberInput width="full" step={0.25} min={0} max={5}>
-                    <NumberInputField />
-                    <NumberInputStepper>
-                      <NumberIncrementStepper />
-                      <NumberDecrementStepper />
-                    </NumberInputStepper>
-                  </NumberInput>
-                </VStack>
-              </Flex>
-            </DrawerBody>
-
-            <DrawerFooter>
-              <Button variant="outline" mr={3} onClick={onClose}>
-                Cancelar
-              </Button>
-              <Button backgroundColor="blueX.500" color="white">
-                Filtrar
-              </Button>
-            </DrawerFooter>
-          </DrawerContent>
-        </Drawer> */}
-      </Flex>
-    </Box>
+      {isOpen && <DrawerFilter isOpen={isOpen} onClose={onClose} />}
+    </Flex>
   );
 }
