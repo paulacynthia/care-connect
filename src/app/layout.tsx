91efd4f2830f0d1562ab36f9478a784852385e6f
paulacynthia <pcynthia013@gmail.com>
2024-07-21 10:27:07 -0300

    :sparkles: feat: root layout metadata & auth provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { AuthProvider } from "@/providers/auth-provider";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Providers } from "./providers";
@@ -5,12 +6,15 @@ import { Providers } from "./providers";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Care Connect",
+  title: {
+    template: "%s | Care Connect",
+    default: "Care Connect",
+  },
   description:
     "Care Connect é um marketplace inovador dedicado a conectar enfermeiros a oportunidades de trabalho em unidades de saúde.",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
@@ -18,7 +22,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Providers>{children}</Providers>
+        <AuthProvider>
+          <Providers>{children}</Providers>
+        </AuthProvider>
       </body>
     </html>
   );
